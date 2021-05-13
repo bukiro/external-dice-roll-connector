3fed40d15f5ad189fcddd124ad41bbab82a2de80
bukiro <arne.x.brandt@outlook.com>
2021-05-13 22:21:11 +0200

    Error notification on error

--- external-dice-roll-connector.js
+++ external-dice-roll-connector.js
@@ -17,34 +17,38 @@ Hooks.on("init", function () {
 
         switch (event.type) {
             case "roll": {
-                //Check if this is a formula or a Roll.
-                //A Roll will be converted into a real Roll object based on its formula, and then overwritten with its attributes where possible.
-                //A Roll Object has getter functions and can't be assigned in one go.
-                if (event.roll.class === "Roll") {
-                    r = new Roll(event.roll.formula);
-                    r.evaluate();
-                    //Overwrite each attribute that isn't a getter.
-                    Object.keys(event.roll).forEach(key => {
-                        try {
-                            r[key] = event.roll[key];
-                        } catch { }
+                try {
+                    //Check if this is a formula or a Roll.
+                    //A Roll will be converted into a real Roll object based on its formula, and then overwritten with its attributes where possible.
+                    //A Roll Object has getter functions and can't be assigned in one go.
+                    if (event.roll.class === "Roll") {
+                        r = new Roll(event.roll.formula);
+                        r.evaluate();
+                        //Overwrite each attribute that isn't a getter.
+                        Object.keys(event.roll).forEach(key => {
+                            try {
+                                r[key] = event.roll[key];
+                            } catch { }
+                        })
+                        //Now turn every term with class === "Die" into a real Die object.
+                        if (event.roll.terms) {
+                            r.terms = event.roll.terms.map(term => term.class === "Die" ? Object.assign(new Die(), term) : term);
+                        }
+                        //If the fake Roll has a total value, apply it to the real Roll's _total (as total is a getter function.)
+                        if (event.roll.total) {
+                            r._total = event.roll.total;
+                        }
+                    } else {
+                        r = new Roll(event.roll.toString());
+                        r.evaluate();
+                    }
+                    //Finally, post the finished Roll into the chat.
+                    r.toMessage({
+                        user: game.user._id,
                     })
-                    //Now turn every term with class === "Die" into a real Die object.
-                    if (event.roll.terms) {
-                        r.terms = event.roll.terms.map(term => term.class === "Die" ? Object.assign(new Die(), term) : term);
-                    }
-                    //If the fake Roll has a total value, apply it to the real Roll's _total (as total is a getter function.)
-                    if (event.roll.total) {
-                        r._total = event.roll.total;
-                    }
-                } else {
-                    r = new Roll(event.roll.toString());
-                    r.evaluate();
+                } catch (error) {
+                    ui.notifications.error("[External Dice Roll Connector] Error: " + error.toString());
                 }
-                //Finally, post the finished Roll into the chat.
-                r.toMessage({
-                    user: game.user._id,
-                })
                 break;
             }
         }
